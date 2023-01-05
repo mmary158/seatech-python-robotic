952d2baa046e1c86a50b9a15512c9bd62aaa5427
mmary158 <marylore162000@gmail.com>
2023-01-05 16:35:19 +0100

    commencer

---
+++ exo1/exo1.py
@@ -0,0 +1,30 @@
+class Robot():
+    __name = "<unnamed>"
+    __power = False
+    __current_speed = 0
+    __battery_level = 0
+    __states = ['shutown', 'running']
+    
+        
+    """
+      Give your best code here ( •̀ ω •́ )✧
+    """
+    def __init__(self,name=None,power=False,current_speed=0,battery_level=0,states=[]):
+        if name:
+            self.__name=name
+            self.__power=False
+            self.__current_speed=__current_speed
+            self.__battery_level=__battery_level
+            self.__states=self.__states[0]
+    	
+    def onoff(self):
+      if self.__power==False:self.__power=True
+      if self.__power==True:self.__power=False
+      print("Le Robot est %s" %(self.__states[self.__power]))
+      pass
+
+	
+    def charge(self):
+      if self.__battery_level==0:self.__battery_level=100
+      print("La batterie est chargé à %s"%(self.battery_level))
+      pass
