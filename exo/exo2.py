e58e4b6857c6190aa3071d4a04651e7388a65d6f
mmary158 <121880393+mmary158@users.noreply.github.com>
2023-01-07 17:32:25 +0100

    Add files via upload

--- exo/exo2.py
+++ exo/exo2.py
@@ -1,21 +1,37 @@
 """
 * RÃ©utiliser la class Robot faite dans l'exo 1. *Sans copier/coller dans le fichier ;)*
-* Un humain doit possÃ©der un sexe attribuable Ã  sa crÃ©ation
+* Un humain doit posséder un sexe attribuable à  sa création
 * Un humain doit pouvoir manger un ou plusieurs aliments
-* Un humain doit pouvoir digÃ©rer ce qu'il a mangÃ© *pas trÃ¨s important, faire en dernier si vous avez le temps*
-* Un Cyborg doit Ãªtre une combinaison d'un humain et d'un robot
-* Bonus : ajouter une mÃ©thode fun au Cyborg
+* Un humain doit pouvoir digérer ce qu'il a mangé *pas très important, faire en dernier si vous avez le temps
+* Un Cyborg doit être une combinaison d'un humain et d'un robot
+* Bonus : ajouter une méthode fun au Cyborg
 """
 from exo1 import Robot
 
-class Robot():
-     #Robot class content here
-    pass
+# class Robot():
+#      #Robot class content here
+#     pass
 
 class Human():   
     sexe=None
-    pass
-
+    estomavide=False
+    estoma=[]
+    
+    def __init_(self,sexe):
+        if sexe:
+            self.sexe=sexe
+    
+    def eat(self,aliment):
+        if self.estomavide==True:
+            self.estoma.append(aliment)
+            self.digest()
+    
+    def digest(self):
+        #4 h dans l'estomac 
+        # 4h intestin grêle
+        # 16h dans le gros intestin
+        
+        
 class Cyborg(Robot, Human):
     name="<unnamed>"   
     sexe="<unnamed>"
@@ -24,11 +40,11 @@ class Cyborg(Robot, Human):
             Robot.__init__(self,name)
             Human.__init__(self,sexe)
 
-
+"""
 cyborg = Cyborg('Deux Ex Machina','M')
 
 print(cyborg.name, 'sexe', cyborg.sexe)
-"""
+
 print('Charging battery...')
 cyborg.charge()
 cyborg.status()
