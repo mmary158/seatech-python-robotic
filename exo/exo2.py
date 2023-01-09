a817cf6a5b72e76c0a7ab469816f9910662f9510
mmary158 <121880393+mmary158@users.noreply.github.com>
2023-01-09 12:03:53 +0100

    Add files via upload

--- exo/exo2.py
+++ exo/exo2.py
@@ -1,54 +1,69 @@
 """
 * RÃ©utiliser la class Robot faite dans l'exo 1. *Sans copier/coller dans le fichier ;)*
-* Un humain doit posséder un sexe attribuable à  sa création
+* Un humain doit possÃ©der un sexe attribuable Ã   sa crÃ©ation
 * Un humain doit pouvoir manger un ou plusieurs aliments
-* Un humain doit pouvoir digérer ce qu'il a mangé *pas très important, faire en dernier si vous avez le temps
-* Un Cyborg doit être une combinaison d'un humain et d'un robot
-* Bonus : ajouter une méthode fun au Cyborg
+* Un humain doit pouvoir digÃ©rer ce qu'il a mangÃ© *pas trÃ¨s important, faire en dernier si vous avez le temps
+* Un Cyborg doit Ãªtre une combinaison d'un humain et d'un robot
+* Bonus : ajouter une mÃ©thode fun au Cyborg
 """
+import time
 from exo1 import Robot
-
-# class Robot():
-#      #Robot class content here
-#     pass
 
 class Human():   
     sexe=None
-    estomavide=False
     estoma=[]
     
-    def __init_(self,sexe):
+    def __init__(self,sexe):
         if sexe:
             self.sexe=sexe
     
     def eat(self,aliment):
-        if self.estomavide==True:
-            self.estoma.append(aliment)
-            self.digest()
+        self.estoma.append(aliment)
+        print("aliment mangÃ©:%s"%(self.estoma))
     
     def digest(self):
-        #4 h dans l'estomac 
-        # 4h intestin grêle
-        # 16h dans le gros intestin
-        
-        
-class Cyborg(Robot, Human):
-    name="<unnamed>"   
-    sexe="<unnamed>"
+        while self.estoma!=[]:
+            a=self.estoma[0]
+            if a!=[]:
+                self.estoma.remove(a)
+                print("aliment digÃ©rÃ©:%s"%(a))
+            time.sleep(4)
+            
+
+
+
+class Cyborg(Robot,Human):
+    courir=False
+    obstacleface=False
+    obstacledroite=False
+    obstaclegauche=False
+    saut=False
+
     def __init__(self,name,sexe):
         if name:   
             Robot.__init__(self,name)
             Human.__init__(self,sexe)
 
-"""
-cyborg = Cyborg('Deux Ex Machina','M')
+    def parcours(self):
+        self.courir=True
+        if self.obstacleface==True:
+            if self.obstacledroite==True and self.obstaclegauche==True:
+                
 
-print(cyborg.name, 'sexe', cyborg.sexe)
 
-print('Charging battery...')
-cyborg.charge()
-cyborg.status()
-cyborg.eat('banana')
-cyborg.eat(['coca', 'chips'])
-cyborg.digest()
-"""
+
+    
+
+if __name__=='__main__':
+    cyborg = Cyborg('Deux Ex Machina','M')
+    cyborg.etat()
+    print("sexe:%s"%(cyborg.sexe))
+   # cyborg.charge()
+    cyborg.eat('Lola')
+    cyborg.eat('chocolatine')
+    cyborg.eat('chat')
+    cyborg.eat('chien')
+    cyborg.eat('hamster')
+    cyborg.digest()
+
+    
