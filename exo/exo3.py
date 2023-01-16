d600c36a675317e94e69f86a5d006dbb12b0b901
mmary158 <marylore162000@gmail.com>
2023-01-16 10:50:08 +0100

    fait

--- exo/exo3.py
+++ exo/exo3.py
@@ -17,57 +17,138 @@ Sortez un bon vieux crayon pour schématiser vos dépendances d'héritages
 
 print("PRINCIPE DE CLASSE ABSTRAITE")
 	
-from abc import ABCMeta
-from abc import abstractmethod
+from abc import ABCMeta,abstractmethod
+import time
 
 class UnmannedVehicle(metaclass=ABCMeta):
     """ 
         An autonomous vehicle have to do his mission automatically.
         This mission can be configured by an operator.
     """
+    @abstractmethod
+    def debut_mission(self,mission):
+        pass
+
+    @abstractmethod
+    def fin_mission(self):
+        pass
+
     pass
 
 class AerialVehicle(metaclass=ABCMeta):
     """ A vehicle made for ground fields."""
+    @abstractmethod
+    def debut_vol(self):
+        pass
+
+    @abstractmethod
+    def looping(self):
+        pass
+
+    @abstractmethod
+    def fin_vol(self):
+        pass
+
     pass
 
 class GroundVehicle(metaclass=ABCMeta):
     """ A vehicle made for ground fields."""
+
+    @abstractmethod
+    def mise_en_marche(self):
+        pass
+    
+    @abstractmethod
+    def arret(self):
+        pass
     pass
 
 class UnderseaVehicle(metaclass=ABCMeta):
     """ A vehicle made for ground fields."""
+
+    @abstractmethod
+    def plonger(self):
+        pass
+
+    def duree(self):
+        print("temps pouvant rester sous l'eau")
+        time.sleep(5)
+        self.sorti()
+        self.plonger()
+        pass
+
+    @abstractmethod
+    def sorti(self):
+        pass
+
     pass
 
-class UAV():
+class UAV(UnmannedVehicle,AerialVehicle):
     
-    def do_something_interesting(self):
-        pass
-    def do_something_aerial_specific(self):
-        pass
+    def debut_vol(self):
+        print("UAV vole")
 
-class UUV():
-    def do_something_interesting(self):
-        pass
-    def do_something_undersea_specific(self):
-        pass
+    def looping(self):
+        print("UAV looping")
 
-class UGV():
-    def do_something_interesting(self):
-        pass
-    def do_something_ground_specific(self):
-        pass
+    def fin_vol(self):
+        print("UAV à terre")
+
+    def debut_mission(self, mission):
+        print('Début de mission :', mission)
+        self.debut_vol()
+        self.looping()
+
+    def fin_mission(self):
+        print('Fin de mission')
+        self.fin_vol()
+
+class UUV(UnmannedVehicle,UnderseaVehicle):
+    
+    def plonger(self):
+        print("UUV en plongé")
+    
+    def sorti(self):
+        print("UUV sorti")
+
+    def debut_mission(self, mission):
+        print('Début de mission :', mission)
+        self.plonger()
+        self.duree()
+
+    def fin_mission(self):
+        print('Fin de mission')
+        self.sorti()
+
+class UGV(UnmannedVehicle,GroundVehicle):
+
+    def mise_en_marche(self):
+        print("UGV en marche")
+    
+    def arret(self):
+        print("UGV en arrêt")
+
+    def debut_mission(self, mission):
+        print('Début de mission :', mission)
+        self.mise_en_marche()
+
+    def fin_mission(self):
+        print('Fin de mission')
+        self.arret()
 
 
 if __name__ == '__main__':
     uav = UAV()
-    uav.do_something_interesting()
-    uav.do_something_aerial_specific()
+    uav.debut_mission("Détection des principaux ennemis sur le terrain:Adrien, Coline, Le Rouquin, Marie")
+    uav.fin_mission()
+    print()
 
     ugv = UGV()
-    ugv.do_something_interesting()
-    ugv.do_something_ground_specific()
+    ugv.debut_mission("transport de 100 millions de scies commandés par Florian")
+    ugv.fin_mission()
+    print()
 
     uuv = UUV()
-    uuv.do_something_interesting()
-    uuv.do_something_undersea_specific()
+    uuv.debut_mission("Exploration sous-marine effectué par Lola et ses chats")
+    uuv.fin_mission()
+    print()
